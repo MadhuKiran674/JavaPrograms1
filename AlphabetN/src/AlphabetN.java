6340fa4040d4542f2fc6fcdca3bb2445a54e4b10
madhu <madhukiran67346@gmail.com>
2024-05-17 21:43:46 +0530

    Alphabet

---
+++ AlphabetN/src/AlphabetN.java
@@ -0,0 +1,21 @@
+
+public class AlphabetN {
+
+
+public static void main(String args[])
+{
+char ch='A';
+while(ch<=90){
+int a=k(ch);
+System.out.println("Asci value="+a+":"+ch);
+ch++;
+}
+}
+public static int k(int res)
+{
+
+
+return res;
+}
+}
+
---
+++ AlphabetN/src/AsciValu.java
@@ -0,0 +1,15 @@
+
+public class AsciValu {
+
+	public static void main(String args[]) {
+		int ch=65;
+		char i=(char) ch;
+		System.out.println(i);
+		
+		
+		char A='A';
+		int j=A;
+	
+		System.out.println(j);
+	}
+}
